0451359aa00d679c23ea873cc87c27594d5fae88
Saulo Tauil <saulo.tauil@qx3.com.br>
2016-06-02 13:14:59 -0700

    Initial Commit

---
+++ BTLE_Poc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTLE_Poc
+//
+//  Created by Saulo Tauil on 2016-06-02.
+//  Copyright © 2016 Saulo Tauil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BTLE_PocTests/BTLE_PocTests.swift
@@ -0,0 +1,36 @@
+//
+//  BTLE_PocTests.swift
+//  BTLE_PocTests
+//
+//  Created by Saulo Tauil on 2016-06-02.
+//  Copyright © 2016 Saulo Tauil. All rights reserved.
+//
+
+import XCTest
+@testable import BTLE_Poc
+
+class BTLE_PocTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
